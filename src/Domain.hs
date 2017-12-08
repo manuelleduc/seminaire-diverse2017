1d685171487fccc7ab80d8ad2c5950687fc5b335
Manuel Leduc <manuel.leduc@inria.fr>
2017-12-08 13:13:03 +0100

    Login error managment + token generation and persistence

--- src/Domain.hs
+++ src/Domain.hs
@@ -1,26 +1,28 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeOperators              #-}
 
-module Domain (User(User),Token(Token,TokenError)) where
+module Domain (User(User), AuthError(AuthError)) where
 
 import           Data.Aeson
 import           Data.Aeson.TH
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant
+import GHC.Generics (Generic)
 
 data User = User
   { login :: String
   , pass  :: String
   } deriving (Eq, Show)
 
-
-data Token = Token {
-  tokenValue :: String
-} | TokenError {
-  fault :: String
-} deriving (Eq, Show)
+newtype AuthError = AuthError { fault :: String }
+  deriving (Show, Generic, Eq, Ord)
 
 $(deriveJSON defaultOptions ''User)
-$(deriveJSON defaultOptions ''Token)
+$(deriveJSON defaultOptions ''AuthError)
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE FlexibleContexts     #-}
+
 
 module Lib
     ( startApp
@@ -12,7 +14,7 @@ module Lib
 
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger     (runStderrLoggingT)
-import           Domain                   (Token (..), User (User))
+import           Domain                   (User (User), AuthError(AuthError)) --Token (..),
 
 import           Data.String.Conversions
 
@@ -32,9 +34,13 @@ import           Data.Time.Clock          (getCurrentTime)
 
 import           Text.StringRandom        (stringRandomIO)
 
+import Control.Monad.Error.Class (MonadError)
 
--- TODO: save tokens as persistent
-type API = "login" :> ReqBody '[JSON] User :> Post '[JSON] Token
+import qualified Data.ByteString.Lazy.Char8 as BS (pack)
+
+import Control.Monad ((<=<))
+
+type API = "login" :> ReqBody '[JSON] User :> Post '[JSON] D.Token
 
 startApp :: FilePath ->  IO ()
 startApp sqliteFile = run 8080 =<< mkApp sqliteFile
@@ -52,13 +58,23 @@ api :: Proxy API
 api = Proxy
 
 server :: ConnectionPool -> Server API
-server pool = loginPostH
-  where loginPostH user = liftIO $ loginPost pool user
+server pool =  loginPostH
+  where
+    loginPostH = withError <=< liftIO . loginPost pool
 
-loginPost:: ConnectionPool -> User -> IO Token
+{-}  do a <- withError <=< loginPostH
+                 return a
+  where loginPostH u = loginPost pool u-}
+
+withError :: MonadError ServantErr m => Either AuthError a -> m a
+withError (Right a) = return a
+withError (Left (AuthError t)) = throwError $ err401 { errBody = BS.pack t }
+
+loginPost:: ConnectionPool -> User -> IO (Either AuthError D.Token)
 loginPost pool (User "admin" "admin") = flip runSqlPersistMPool pool $ do
       t <- liftIO getCurrentTime
       r <- liftIO $ stringRandomIO "[a-zA-Z0-9]{50}"
-      insert (D.Token r t)
-      return $ Token (unpack r)
-loginPost _ _ = return $ TokenError "invalid login and password"
+      let token = D.Token r t
+      insert token
+      return $ Right token
+loginPost _ _ = return $ Left (AuthError "invalid login and password") -- $ err401 undefined --return $ TokenError "invalid login and password"
